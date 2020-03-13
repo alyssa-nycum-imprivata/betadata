0ba78df63ec2ac64a952720a26517f7765ba664b
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-03-13 16:58:21 -0500

    added conditional to sign up form to check if email and username are already in use; also add second password field and conditional to check if both passwords match

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -4,34 +4,60 @@ import UserApiManager from '../../modules/UserApiManager';
 
 const SignUp = (props) => {
     const [newUser, setNewUser] = useState({ email: "", username: "", password: "", first_name: "", last_name: "" });
+    const [confirmedPassword, setConfirmedPassword] = useState({ password_2: "" });
     const [isLoading, setIsLoading] = useState(false);
 
     const handleSignUpFieldChange = (evt) => {
         const stateToChange = { ...newUser };
+        if (evt.target.id.includes('password')) {
+            stateToChange[evt.target.id.split("_")[0]] = evt.target.value;
+            setNewUser(stateToChange);
+        } else {
+            stateToChange[evt.target.id] = evt.target.value;
+            setNewUser(stateToChange);
+        }
+    };
+
+    const handleConfirmedPassword = (evt) => {
+        const stateToChange = { ...confirmedPassword };
         stateToChange[evt.target.id] = evt.target.value;
-        setNewUser(stateToChange);
+        setConfirmedPassword(stateToChange);
     };
 
     const handleSignUp = (e) => {
         e.preventDefault();
 
-        if (newUser.email === "" || newUser.username === "" || newUser.password === "" || newUser.first_name === "" || newUser.last_name === "") {
-            window.alert("Please fill out all fields");
-        } else {
-            setIsLoading(true);
+        const password1 = newUser.password;
+        const password2 = confirmedPassword.password_2;
 
-            const newUserObject = {
-                id: props.match.params.userId,
-                email: newUser.email,
-                username: newUser.username,
-                password: newUser.password,
-                first_name: newUser.first_name,
-                last_name: newUser.last_name
+        UserApiManager.getUsers().then(users => {
+            const emails = users.filter(user => (newUser.email === user.email));
+            const usernames = users.filter(user => (newUser.username === user.username));
+            if (emails.length !== 0) {
+                window.alert("Email already taken! Please try again.");
+            } else if (usernames.length !== 0) {
+                window.alert("Username already taken! Please try again.");
+            } else if (password1 !== password2) {
+                window.alert("Passwords don't match! Please try again");
+            } else if (newUser.email === "" || newUser.username === "" || newUser.password === "" || newUser.first_name === "" || newUser.last_name === "") {
+                window.alert("Please fill out all fields");
+            } else {
+                setIsLoading(true);
+
+                const newUserObject = {
+                    id: props.match.params.userId,
+                    email: newUser.email,
+                    username: newUser.username,
+                    password: newUser.password,
+                    first_name: newUser.first_name,
+                    last_name: newUser.last_name
+                };
+
+                UserApiManager.postUser(newUserObject)
+                    .then(window.alert("Account creation successful! Now, please login."))
+                    .then(() => props.history.push("/"));
             };
-
-            UserApiManager.postUser(newUserObject)
-                .then(() => props.history.push("/"));
-        };
+        });
     };
 
     return (
@@ -40,7 +66,7 @@ const SignUp = (props) => {
                 <h3>Please sign up</h3>
                 <div className="signUp-form-fields">
 
-                    <label htmlFor="inputEmail"id="email-label">Email address:</label>
+                    <label htmlFor="inputEmail" id="email-label">Email address:</label>
                     <input onChange={handleSignUpFieldChange}
                         type="email"
                         id="email"
@@ -80,11 +106,19 @@ const SignUp = (props) => {
                         required=""
                         autoFocus="" />
 
+                    <label htmlFor="inputReEnteredPassword" id="reEnteredPassword-label">Re-Enter Password:</label>
+                    <input onChange={handleConfirmedPassword}
+                        type="password"
+                        id="password_2"
+                        placeholder="Re-Enter Password"
+                        required=""
+                        autoFocus="" />
+
                 </div>
                 <button type="submit"
                     disabled={isLoading}
                     id="signUp-button">Sign Up</button>
-                <button id="signUp-cancel-button" onClick={() => {props.history.push("/")}}>Cancel</button>
+                <button id="signUp-cancel-button" onClick={() => { props.history.push("/") }}>Cancel</button>
             </fieldset>
         </form>
     );
