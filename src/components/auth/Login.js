8db0db834dc9f1d32a058f32eb12937863d26265
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-03-13 11:37:47 -0500

    made sign up form

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -27,7 +27,7 @@ const Login = (props) => {
                     <h3>Please login</h3>
                     <div className="login-form">
 
-                        <label htmlFor="inputEmail" id="email-label">Email address:</label>
+                        <label htmlFor="inputEmail" id="email-label-login">Email address:</label>
                         <input onChange={handleFieldChange}
                             type="email"
                             id="email"
@@ -35,7 +35,7 @@ const Login = (props) => {
                             required=""
                             autoFocus="" />
 
-                        <label htmlFor="inputPassword" id="password-label">Password:</label>
+                        <label htmlFor="inputPassword" id="password-label-login">Password:</label>
                         <input onChange={handleFieldChange}
                             type="password"
                             id="password"
@@ -45,6 +45,56 @@ const Login = (props) => {
 
                     </div>
                     <button type="submit" id="login-button">Login</button>
+                </fieldset>
+            </form>
+
+            <form onSubmit={handleLogin} id="signUp-form">
+                <fieldset>
+                    <h3>Please sign up</h3>
+                    <div className="signUp-form-fields">
+
+                        <label htmlFor="inputEmail" id="email-label">Email address:</label>
+                        <input onChange={handleFieldChange}
+                            type="email"
+                            id="newEmail"
+                            placeholder="Email address"
+                            required=""
+                            autoFocus="" />
+
+                        <label htmlFor="inputUsername" id="username-label">Username:</label>
+                        <input onChange={handleFieldChange}
+                            type="text"
+                            id="username"
+                            placeholder="Username"
+                            required=""
+                            autoFocus="" />
+
+                        <label htmlFor="inputFirstName" id="firstName-label">First Name:</label>
+                        <input onChange={handleFieldChange}
+                            type="text"
+                            id="first_name"
+                            placeholder="First Name"
+                            required=""
+                            autoFocus="" />
+
+                        <label htmlFor="inputLastName" id="lastName-label">Last Name:</label>
+                        <input onChange={handleFieldChange}
+                            type="text"
+                            id="last_name"
+                            placeholder="Last Name"
+                            required=""
+                            autoFocus="" />
+
+                        <label htmlFor="inputPassword" id="password-label">Password:</label>
+                        <input onChange={handleFieldChange}
+                            type="password"
+                            id="newPassword"
+                            placeholder="Password"
+                            required=""
+                            autoFocus="" />
+
+                    </div>
+                    <button type="submit" id="signUp-button">Sign Up</button>
                 </fieldset>
             </form>
         </>
