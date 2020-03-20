ea5429259aefb7707b2abb620e3f54813bb5a733
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-03-20 16:04:15 -0500

    is_flashed on add climb form was changed into a dropdown

--- src/components/attempts/AttemptCard.js
+++ src/components/attempts/AttemptCard.js
@@ -6,17 +6,18 @@ const AttemptCard = (props) => {
             <>
                 <div className="each-attempt">
                     <h4>{props.attempt.attempt_date} -- </h4>
-                    <h4>{props.attempt.is_flashed ? "Flashed" : null}</h4>
-                    <h4>{props.attempt.is_flashed || props.attempt.is_clean ? null : "Falls: " + props.attempt.number_of_falls}</h4>
-                    <h4>{props.attempt.is_clean ? "Cleaned" : null}</h4>
+                    {props.attempt.is_flashed === true ? <h4>Flashed</h4> : null }
+                    {props.attempt.number_of_falls !== 0 ? <h4>Falls: {props.attempt.number_of_falls}</h4> : null}
+                    {props.attempt.is_clean === true ? <h4>Cleaned</h4> : null }
                 </div>
                 <div className="attempt-buttons">
                     {/* <button type="button" className="edit-attempt-button" onClick={() => { props.history.push(`/attempts/${props.attempt.id}/edit`) }}>Edit</button> */}
-                    {!props.climb.is_archived ? 
-                    <button type="button" className="delete-attempt-button" onClick={() => {
-                        props.handleAttemptDelete(props.attempt.id)}}> Delete</button>
-                    : null }
-                            </div>
+                    {!props.climb.is_archived ?
+                        <button type="button" className="delete-attempt-button" onClick={() => {
+                            props.handleAttemptDelete(props.attempt.id)
+                        }}> Delete</button>
+                        : null}
+                </div>
             </>
         </div>
     )
