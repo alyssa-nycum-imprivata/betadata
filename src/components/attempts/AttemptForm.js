e4724131d7adbe4d39f6bf0f25e6545236d6ca20
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-03-20 17:14:22 -0500

    is_clean checkbox was changed to a dropdown on the add attempt form

--- src/components/attempts/AttemptForm.js
+++ src/components/attempts/AttemptForm.js
@@ -2,8 +2,7 @@ import React, { useState } from 'react';
 import AttemptApiManager from '../../modules/AttemptApiManager';
 
 const AttemptForm = (props) => {
-    const [attempt, setAttempt] = useState({ climbId: "", attempt_date: "", number_of_falls: 0, is_flashed: false });
-    const [checkbox, setCheckbox] = useState(false)
+    const [attempt, setAttempt] = useState({ climbId: "", attempt_date: "", number_of_falls: 0, is_flashed: "", is_clean: "" });
     const [isLoading, setIsLoading] = useState(false);
 
     const handleFieldChange = (evt) => {
@@ -12,14 +11,10 @@ const AttemptForm = (props) => {
         setAttempt(stateToChange);
     };
 
-    const handleCheckbox = () => {
-        setCheckbox(!checkbox)
-    }
-
     const constructNewAttempt = (evt) => {
         evt.preventDefault();
-        if (attempt.attempt_date === "") {
-            window.alert("Please fill out attempt date.")
+        if (attempt.attempt_date === "" || attempt.is_clean === "") {
+            window.alert("Please fill out all fields.")
         } else {
             setIsLoading(true);
 
@@ -29,9 +24,9 @@ const AttemptForm = (props) => {
                 attempt_date: attempt.attempt_date,
                 number_of_falls: parseInt(attempt.number_of_falls),
                 is_flashed: attempt.is_flashed,
-                is_clean: checkbox
+                is_clean: JSON.parse(attempt.is_clean)
             };
-    
+
             AttemptApiManager.postAttempt(newAttempt)
                 .then(() => props.history.push("/climbs"));
         }
@@ -50,19 +45,29 @@ const AttemptForm = (props) => {
                             onChange={handleFieldChange}
                         />
 
-                        <label htmlFor="number_of_falls">Number of Falls:</label>
-                        <input type="number"
-                            id="number_of_falls"
+                        <label htmlFor="is_clean">Cleaned?:</label>
+                        <select id="is_clean"
                             required
+                            value={attempt.is_clean}
+                            name="is_clean"
                             onChange={handleFieldChange}
-                        />
+                        >
+                            <option value="" disabled defaultValue>Select</option>
+                            <option value="true">Yes</option>
+                            <option value="false">No</option>
+                        </select>
 
-                        <div className="cleaned">
-                            <label htmlFor="is_clean">Cleaned?:</label>
-                            <input type="checkbox" id="is_clean" onChange={handleCheckbox}
-                            />
-                            <label htmlFor="is_clean">Yes</label>
-                        </div>
+                        {attempt.is_clean === "true" || attempt.is_clean === "" ? null :
+                            <>
+                                <label htmlFor="number_of_falls">Number of Falls:</label>
+                                <input type="number"
+                                    id="number_of_falls"
+                                    required
+                                    onChange={handleFieldChange}
+                                />
+                            </>
+                        }
+
                     </div>
                     <div className="add-attempt-button-container">
                         <button type="button" disabled={isLoading} onClick={constructNewAttempt}>Add</button>
