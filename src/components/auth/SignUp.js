21aeac9a61ff500535bc9d900688bc952ea06b2c
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-03-25 12:28:18 -0500

    changed name of auth css file

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Login.css';
+import './Auth.css';
 import UserApiManager from '../../modules/UserApiManager';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
