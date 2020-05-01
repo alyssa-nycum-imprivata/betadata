00eecf41d2bc75c4d64c1bf35ca65096fb5754b7
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-04-30 22:07:48 -0500

    commented code in login.js and signup.js

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -5,20 +5,24 @@ import UserApiManager from '../../modules/UserApiManager';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 const Login = (props) => {
+    // preparing to set the existing user's info in state
     const [credentials, setCredentials] = useState({ email: "" });
 
+    // listens to what the user inputs into the form fields in real time and sets the user's info in state
     const handleLoginFieldChange = (evt) => {
         const stateToChange = { ...credentials };
         stateToChange[evt.target.id] = evt.target.value;
         setCredentials(stateToChange);
     };
 
+    // gets all users from the database, checks to see if the email entered is one that exists in the database, and sets session storage with the user's info & logs the user into the app if the email does exist
     const handleLogin = (e) => {
         e.preventDefault();
 
         UserApiManager.getUsers().then(users => {
             const userObject = users.filter(user => (credentials.email === user.email));
 
+            // if the entered email does not exist in the database, return a window alert
             if (userObject.length !== 1) {
                 window.alert("Incorrect email. Please try again. If you do not have an account, click the sign up link below.");
             } else {
@@ -29,11 +33,17 @@ const Login = (props) => {
         });
     };
 
+    // returns the login form with an email address input, a link to follow for new users, and a 'login' button
     return (
+        // when the 'login' button is clicked, execute the handleLogin function
         <Form onSubmit={handleLogin} className="login-form">
+
+            {/* form header */}
             <FormGroup className="login-form-header-container">
                 <h2 className="login-form-header">Please Login</h2>
             </FormGroup>
+
+            {/* email input */}
             <FormGroup className="login-form-input-container">
                 <Label htmlFor="inputEmail" className="login-label"><strong>Email Address:</strong></Label>
                 <Input onChange={handleLoginFieldChange}
@@ -44,6 +54,7 @@ const Login = (props) => {
                     autoFocus="" />
             </FormGroup>
 
+            {/* link for new users to follow */}
             <FormGroup className="login-form-note-container">
                 <h6>If you are a new user, <Link className="signUp-link"
                     to={"/signup"}>click here to sign up.</Link></h6>
--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -4,21 +4,29 @@ import UserApiManager from '../../modules/UserApiManager';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 const SignUp = (props) => {
+    // preparing to set the new user's info in state
     const [newUser, setNewUser] = useState({ email: "", username: "", first_name: "", last_name: "" });
     const [isLoading, setIsLoading] = useState(false);
 
+    // listens to what the user inputs into the form fields in real time and sets the user's info in state
     const handleSignUpFieldChange = (evt) => {
         const stateToChange = { ...newUser };
             stateToChange[evt.target.id] = evt.target.value;
             setNewUser(stateToChange);
     };
 
+    // gets all users from the database and runs several checks to compare the info the user entered in with user info already in the database
+    // if the new user's info passes the checks, a new user object is constructed & saved to the database and the user is re-directed to the login page and prompted to login
     const handleSignUp = (e) => {
         e.preventDefault();
 
         UserApiManager.getUsers().then(users => {
+            // checks to see if the new user's email matches an email already stored in the database and returns a window alert if so
             const emails = users.filter(user => (newUser.email === user.email));
+            // checks to see if the new user's username matches a username already stored in the database and returns a window alert if so
             const usernames = users.filter(user => (newUser.username === user.username));
+
+            //  the email, username, first name, and last name fields must be filled out
             if (emails.length !== 0) {
                 window.alert("Email already taken! Please try again.");
             } else if (usernames.length !== 0) {
@@ -36,6 +44,7 @@ const SignUp = (props) => {
                     last_name: newUser.last_name
                 };
 
+                // returns an account successful window alert message and directs the new user to login
                 UserApiManager.postUser(newUserObject)
                     .then(window.alert("Account creation successful! Now, please login."))
                     .then(() => props.history.push("/"));
@@ -43,13 +52,19 @@ const SignUp = (props) => {
         });
     };
 
+    // returns the sign up form with email address, username, first name, & last name inputs and 'sign up' & 'cancel' buttons
     return (
+        // when the 'sign up' button is clicked, execute the handleSignUp function
         <Form onSubmit={handleSignUp} className="signUp-form">
+
+            {/* form header */}
             <FormGroup className="signUp-form-header-container">
                 <h2 className="signUp-form-header">Please Sign Up</h2>
             </FormGroup>
+
             <FormGroup className="signUp-form-input-container">
                 <div className="signUp-form-input-div">
+                    {/* email input */}
                     <div className="signUp-form-input-div-nested">
                         <Label htmlFor="inputEmail" className="signUp-label signUp-label-left"><strong>Email Address:</strong></Label>
                         <Input onChange={handleSignUpFieldChange}
@@ -60,6 +75,7 @@ const SignUp = (props) => {
                             autoFocus="" />
                     </div>
 
+                    {/* username input */}
                     <div className="signUp-form-input-div-nested">
                         <Label htmlFor="inputUsername" className="signUp-label signUp-label-right"><strong>Username:</strong></Label>
                         <Input onChange={handleSignUpFieldChange}
@@ -71,6 +87,7 @@ const SignUp = (props) => {
                     </div>
                 </div>
 
+                {/* first name input */}
                 <div className="signUp-form-input-div">
                     <div className="signUp-form-input-div-nested">
                         <Label htmlFor="inputFirstName" className="signUp-label signUp-label-left"><strong>First Name:</strong></Label>
@@ -82,6 +99,7 @@ const SignUp = (props) => {
                             autoFocus="" />
                     </div>
 
+                    {/* last name input */}
                     <div className="signUp-form-input-div-nested">
                         <Label htmlFor="inputLastName" className="signUp-label signUp-label-right"><strong>Last Name:</strong></Label>
                         <Input onChange={handleSignUpFieldChange}
@@ -97,6 +115,7 @@ const SignUp = (props) => {
 
             <FormGroup className="signUp-form-button-container">
                 <Button type="submit" disabled={isLoading} className="signUp-form-button signUp-form-signUp-button" size="lg">Sign Up</Button>
+                {/* when the 'cancel' button is clicked, re-direct to the login form page */}
                 <Button type="button" className="signUp-form-button signUp-form-cancel-button" size="lg" onClick={() => { props.history.push("/") }}>Cancel</Button>
             </FormGroup>
         </Form>
